30986b54246876dff983801f66e53bf7e9b75c20
VasayXTX <VasayXTX@mail.ru>
2011-02-17 23:51:41 +1000

    Changing printer.cpp

--- printer.cpp
+++ printer.cpp
@@ -12,24 +12,28 @@ void Printer::PrintText(const std::string &aText) const
 
 void Printer::PrintAbout() const
 {
-	std::cout << "Packer v1.0.\nCopyright(c) Vasiliy Tsubenko\n";
+	std::cout << "Packer v1.0. Program for compression data\n";
+	std::cout << "Copyright(c) Vasiliy Tsubenko\n";
 	std::cout << "For more information, run program with key \"/h\"\n";
 }
 
 void Printer::PrintHelp() const
 {
-	std::cout << "Packer v1.0.\nCopyright(c) Vasiliy Tsubenko\n";
-	std::cout << "Program for compression data\n\n";
-	std::cout << "How to use program for compression:\n";
-	std::cout << "> Packer.exe /c ALG ARCHIVE_NAME FILE_LIST\n";
-	std::cout << "ALG:\n";
-	std::cout << "\t/r\tcompression using RLE\n";
-	std::cout << "\t/h\tcompression using Huffman\n";
-	std::cout << "ARCHIVE_NAME:\n\tName of the archived file\n";
-	std::cout << "LIST_FILE\n\tList of files for compressing (FILE_NAME1, FILE_NAME2, ..)\n\n";
-	std::cout << "How to use program for decompression:\n";
-	std::cout << "> Packer.exe /d ARCHIVE_NAME\n";
-	std::cout << "ARCHIVE_NAME:\n\tName of the archived file\n";
+	std::cout << "Packer v1.0. Program for compression data\n";
+	std::cout << "Copyright(c) Vasiliy Tsubenko\n\n";
+	std::cout << "\tHow to use program for compression:\n";
+	std::cout << "\t> Packer.exe /c ALG ARCHIVE_FILE_NAME LIST_FILE_NAME\n";
+	std::cout << "\t\tALG:\n";
+	std::cout << "\t\t\t/r\tcompression using RLE\n";
+	std::cout << "\t\t\t/h\tcompression using Huffman\n";
+	std::cout << "\t\tARCHIVE_FILE_NAME:\n";
+	std::cout << "\t\t\tName of archived file\n";
+	std::cout << "\t\tLIST_FILE_NAME:\n";
+	std::cout << "\t\t\tList of files for compressing (FILE_NAME1, FILE_NAME2, ..)\n\n";
+	std::cout << "\tHow to use program for decompression:\n";
+	std::cout << "\t> Packer.exe /d ARCHIVE_FILE_NAME\n";
+	std::cout << "\t\tARCHIVE_FILE_NAME:\n";
+	std::cout << "\t\t\tName of archived file";
 }
 
 void Printer::PrintError(const Error &aErr) const
